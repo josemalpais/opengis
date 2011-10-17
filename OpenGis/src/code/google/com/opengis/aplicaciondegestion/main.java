8ec17b11812810eb402d0eafa273e466ac5a3389
jugade92 <jugade92@gmail.com>
2011-10-17 10:30:07 +0000

    Actualización

--- OpenGis/src/code/google/com/opengis/aplicaciondegestion/main.java
+++ OpenGis/src/code/google/com/opengis/aplicaciondegestion/main.java
@@ -2,11 +2,12 @@ package code.google.com.opengis.aplicaciondegestion;
 
 public class main {
 
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hola a todos!");
+		
 
 	}
 
