a476d2cfd13b7b96e31a973b4ef7d3862b13b1a8
jugade92 <jugade92@gmail.com>
2011-10-20 11:54:03 +0000

    Creación de la Clase Usuarios. La cual se encarga de validar todos los campos introducidos desde nuestra Interfaz Gráfica.

--- OpenGis/src/code/google/com/opengis/aplicaciondegestion/main.java
+++ OpenGis/src/code/google/com/opengis/aplicaciondegestion/main.java
@@ -1,15 +1,14 @@
 package code.google.com.opengis.aplicaciondegestion;
 
+import javax.swing.JOptionPane;
+
 public class main {
 
 
 	public static void main(String[] args) {
 		
+
 		
-		System.out.println("Hola a todos!");
-		System.out.println("Adios a todos!");
-		System.out.println("Adios a todos!");
-		System.out.println("Hola soy edu");
 	}
 
 }
