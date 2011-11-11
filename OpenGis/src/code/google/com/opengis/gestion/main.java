b29c110b81453f814c21677ba568edec71971b0f
Pipepito <pipepito@gmail.com>
2011-11-11 18:12:21 +0000

    Hemos modificado la clase conexión entre Dani y yo, ahora ya funciona, aunque falta el método para hacer updates.
    La clase validar login a falta de una prueba ya funciona.

--- OpenGis/src/code/google/com/opengis/gestion/main.java
+++ OpenGis/src/code/google/com/opengis/gestion/main.java
@@ -1,5 +1,11 @@
+
+
 package code.google.com.opengis.gestion;
 
+import java.sql.*;
+import java.sql.SQLException;
+
+import code.google.com.opengis.gestionDAO.ConectarDBA;
 import code.google.com.opengis.gestionVISUAL.VentanaPrincipal;
 
 public class main {
@@ -7,11 +13,24 @@ public class main {
 
 	private static VentanaPrincipal vp;
 
-	public static void main(String[]args) {
+	public static void main(String[]args) throws SQLException {
 		
-
+		
 		System.out.println("Hola a todos! Soy pepe y estoy probando");
 		vp = new VentanaPrincipal();
+		//Lo que está comentado es una consulta para probar la clase conexión. Att. Pepe.
+		
+		/*ConectarDBA dba = new ConectarDBA();
+		dba.acceder();
+		String sentencia = "SELECT * FROM users";
+		ResultSet rs = dba.consulta(sentencia);
+		while (rs.next()){
+			System.out.println(rs.getString(1));
+			
+		}
+		rs.close();
+		dba.cerrarCon();*/
 	}
+	
 
 }
--- OpenGis/src/code/google/com/opengis/gestionDAO/ConectarDBA.java
+++ OpenGis/src/code/google/com/opengis/gestionDAO/ConectarDBA.java
@@ -1,40 +1,79 @@
+/*****************************************************************************
+* Copyright (c) 2011 [OpenGisTeam]                                           *
+******************************************************************************/
 package code.google.com.opengis.gestionDAO;
 import java.sql.*;
 
+/**
+* @author Danico & Pipepito
+* Clase que establece conexión con la base de datos y realiza las diferentes funciones con esta
+* Ya sea Querys o Updates
+* Última modificación 11/11/11
+* Faltan por hacer los métodos para realizar otros executes.
+*/
+
 public class ConectarDBA {
-	Connection conexion = null;
-	private String userdba ;
-	private String passworddba;
-	private String url;
+
+/** Objeto del tipo conexión */
+static Connection conexion;
+/** Objeto del tipo Statement */
+static Statement st;
 	
 
 
+/** 
+ * Método que nos conecta con la base de datos
+*/ 	
+	public static void acceder() {
+		 
+        // Asignamos a variables todos los datos requeridos para la conexion a
+        // la base de datos.
+        String nombreDriver = "com.mysql.jdbc.Driver";
+        String nombreServidor = "db4free.net";
+        String numeroPuerto = "3306";
+        String miBaseDatos = "dai2opengis";
+        String url = "jdbc:mysql://" + nombreServidor + ":" + numeroPuerto
+                        + "/" + miBaseDatos;
+        String dbuser = "ivanserrano";
+        String dbpwd = "dai20112012";
 
-	public void acceder() throws SQLException{
-		 try {
-			Class.forName("com.mysql.jdbc.Driver");
-			
-		    Connection conexion = DriverManager.getConnection(url,userdba,passworddba);
-			
-		} catch (ClassNotFoundException e) {
-			
-			e.printStackTrace();
-		}
-	      
-	  
-	      
-	}
+        try {
+                Class.forName(nombreDriver).newInstance();
+                conexion = DriverManager.getConnection(url, dbuser, dbpwd);
+                st = conexion.createStatement();
+                System.out.println("conectaBD");
+        } catch (IllegalAccessException e) {
+                System.err.println(e.getMessage());
+        } catch (InstantiationException e) {
+                System.err.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+                System.err.println(e.getMessage());    
+        } catch (SQLException e) {
+                System.out.println("akipeta");
+                System.err.println(e.getMessage());
+        }
+}
+/**
+ * 	Método para realizar consultas a la base de datos.
+ * @param sentenciaSQL String consulta que vamos a ejecutar
+ * @return ResultSet devuelve el resultado de la consulta.
+ * @throws SQLException devuelve un error en caso de producirse
+ */
 	public ResultSet consulta(String sentenciaSQL) throws SQLException
 	{
 	//   Creamos un tipo Statement que maneja las consultas 
-	   Statement s = this.conexion.createStatement();
+	   
 	//   Retorno la consulta especifica...
-	    return  s.executeQuery (sentenciaSQL);
+	    return  st.executeQuery (sentenciaSQL);
 	 
 	}
-	
+	/**
+	 * Método que cierra la conexión con la base de datos y lo statements.
+	 * @throws SQLException
+	 */
 	public void cerrarCon() throws SQLException{
-		this.conexion.close();
+		st.close();
+		conexion.close();
 			
 	}
 }
