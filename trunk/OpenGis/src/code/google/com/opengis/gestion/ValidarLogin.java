dee83cc8d02d0c4f18717cb5b843b7309591bdf0
denycow@gmail.com <denycow@gmail.com@1bd2956d-1e62-b8c4-f405-d16e971eae1f>
2011-11-15 12:23:27 +0000

    esta es la clase el tipo de usuario segun los el usuario y la pass  13:22 del dia  15/11/2011 daniel inarejos

--- trunk/OpenGis/src/code/google/com/opengis/gestion/ValidarLogin.java
+++ trunk/OpenGis/src/code/google/com/opengis/gestion/ValidarLogin.java
@@ -1,86 +1,47 @@
 package code.google.com.opengis.gestion;
+import java.io.FileWriter;
 import java.sql.*;
 
 import code.google.com.opengis.gestionDAO.ConectarDBA;
+import code.google.com.opengis.gestionDAO.LoginDao;
 
 
 public class ValidarLogin {
 
-	private String user;
-	private String password;
-	boolean validacion;
+	
+    String user;
+    String password;
+    char tipodato;
 
-	private char tipodato;
-
 	
 	
 	
 	
 
-	//url="http://www.db4free.net/phpMyAdmin/index.php?target=server_databases.php&token=11ac3398ec9e98c883a291c82348fa50#PMAURL:db=dai2opengis&server=1&target=db_structure.php&token=11ac3398ec9e98c883a291c82348fa50"
 	
 	public ValidarLogin(String user,String password) {
 		this.user = user;
 		this.password = password;
+		
 	}
 
 	
-	public String getUser() {
-		return user;
-	}
-
-
-	public void setUser(String user1) {
-		user = user1;
-	}
-
-
-	public String getPassword() {
-		return password;
-	}
-
-
-	public void setPassword(String password1) {
-		password = password1;
-	}
-
-	/*********************
-	 * metodo que se conecta a la dba selecciona el usuario y la contraseña que se le a dado a la clase
-	 * y elije  el tipo de usuario que es.
-	 ********************/
 	
-	public boolean aceptarUser(){
-		try{
-		      
-		     ConectarDBA dba = new ConectarDBA();
-		     dba.acceder();		     
-		     
-		      String sentencia = "SELECT dni_usuario, password, tipo FROM users WHERE dni_usuario LIKE '"+this.user+"'AND password LIKE '"+this.password+"'";
-		      
-		      ResultSet rs = dba.consulta(sentencia);
-		      while (rs.next()){
-			      String tipo = rs.getString(3);
-			      tipodato = tipo.charAt(0);
-			      } 
-			      rs.close();
-			      dba.cerrarCon();
-			      validacion = true;
-		return validacion;}
-		catch(SQLException aoaao){
-			validacion = false;
-			return validacion;
-		     }
-	}
+
+	
+	
+	
 
 	/*********************
-	 * dado el tipo si es true el resultado de validacion elije un tipo
-	 * que es y te habre una ventana o otra
+	 * recibe un char de la clase LoginDao y dado el tipo selecciona una opcion
+	 * @throws SQLException 
 	 ********************/
-	public void validarUser(boolean validacion){
+	public void tipoDeUser() throws SQLException{
 		
 		
+		LoginDao lgo = new LoginDao(this.user, this.password);
+		tipodato = lgo.datosDba();
 		
-		if(validacion==true){
 			
 				
         	    //una vez valide los datos elije el tipo de usuario que es para darle permisos
@@ -95,14 +56,55 @@ public class ValidarLogin {
 					System.out.println(" funciona es  Admin");
 				    break;
 				default:
-					System.out.println("no tiene tipo");
-				};}
-	  else 
-				System.out.println("el user/contraseña no es correcto");
+					System.out.println("el user/contraseña no es correcto");
+				};
+				
 				
 			
 			 
 		}
+
+
+
+
+	public String getUser() {
+		return user;
+	}
+
+
+
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+	public char getTipodato() {
+		return tipodato;
+	}
+
+
+
+
+	public void setTipodato(char tipodato) {
+		this.tipodato = tipodato;
+	}
 		}
 	
 		
