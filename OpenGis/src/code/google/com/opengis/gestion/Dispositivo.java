53eec332e1e1ed59154f0e7a9eb8ffa61d83a488
ivsepa <ivsepa@gmail.com>
2011-11-18 19:32:13 +0000

    Iván Serrano y Jose Alapont han creado el método "modificarDispositivo" de la clase "DispositivoDAO", y terminado los métodos "altaDispositivo" y "borrarDispositivo" de la misma.

--- OpenGis/src/code/google/com/opengis/gestion/Dispositivo.java
+++ OpenGis/src/code/google/com/opengis/gestion/Dispositivo.java
@@ -6,172 +6,142 @@ import javax.swing.JOptionPane;
 
 import code.google.com.opengis.gestionDAO.ConectarDBA;
 
+/**
+ * 
+ * @author Iván Serrano y Jose Alapont
+ * 
+ */
 public class Dispositivo {
 	private String iddispositivo;
 	private String modelo;
 	private String numSerie;
-	//private boolean disponible;
-	//private boolean activo;
-	
-	////////////////////////////////GETTERS Y SETTERS///////////////////
+
+	/**
+	 * ***************GETTERS Y SETTERS
+	 */
 	public String getIddispositivo() {
-		System.out.println("Dispositivo.iddispositivo = " + this.iddispositivo + ".");
+		System.out.println("Dispositivo.iddispositivo = " + this.iddispositivo
+				+ ".");
 		return iddispositivo;
 	}
 
-
-
 	public void setIddispositivo(String iddispositivo) {
 		this.iddispositivo = iddispositivo;
-		System.out.println("Dispositivo.iddispositivo (nuevo) = " + this.iddispositivo + ".");
+		System.out.println("Dispositivo.iddispositivo (nuevo) = "
+				+ this.iddispositivo + ".");
 	}
-
-
 
 	public String getModelo() {
 		System.out.println("Dispositivo.modelo = " + this.modelo + ".");
 		return modelo;
 	}
 
-
-
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 		System.out.println("Dispositivo.modelo (nuevo) = " + this.modelo + ".");
 	}
-
-
 
 	public String getNumSerie() {
 		System.out.println("Dispositivo.numSerie = " + this.numSerie + ".");
 		return numSerie;
 	}
 
-
-
 	public void setNumSerie(String numSerie) {
 		this.numSerie = numSerie;
-		System.out.println("Dispositivo.numSerie (nuevo) = " + this.numSerie + ".");
+		System.out.println("Dispositivo.numSerie (nuevo) = " + this.numSerie
+				+ ".");
 	}
 
-
-
-	/*public boolean isDisponible() {
-		System.out.println("Dispositivo.disponible = " + this.disponible + ".");
-		return disponible;
-	}
-
-
-
-	public void setDisponible(boolean disponible) {
-		this.disponible = disponible;
-		System.out.println("Dispositivo.disponible (nuevo) = " + this.disponible + ".");
-	}
-
-
-
-	public boolean isActivo() {
-		System.out.println("Dispositivo.activo = " + this.activo + ".");
-		return activo;
-	}
-
-
-
-	public void setActivo(boolean activo) {
-		this.activo = activo;
-		System.out.println("Dispositivo.activo (nuevo) = " + this.activo + ".");
-	}*/
-
-	
-	
-	////////////////////////////////	 CONSTRUCTOR 	///////////////////////////////////
-	
-
+	/**
+	 * ***************CONSTRUCTOR
+	 */
 	public Dispositivo(String iddispositivo, String modelo, String numSerie,
 			boolean disponible, boolean activo) throws SQLException {
 		super();
 		boolean b1 = this.validarDatos(iddispositivo, modelo, numSerie);
-		if(b1 = true){
-			
-		this.iddispositivo = iddispositivo;
-		this.modelo = modelo;
-		this.numSerie = numSerie;
-		//this.disponible = disponible;
-		//this.activo = activo;
-		
-		/*ConectarDBA dba = new ConectarDBA();
-		dba.acceder();
-		//String sentencia = "INSERT INTO `dai2opengis`.`dispositivo` (`iddispositivo` ,`modelo` ,`num_serie` ,`disponible` ,`activo`) VALUES (`" + this.iddispositivo + "`, `" + this.modelo + "`, `" + this.numSerie + "`, `" + this.disponible + "`, `" + this.activo + "`)";
-		String sentencia = "INSERT INTO `dai2opengis`.`dispositivo` (`iddispositivo` ,`modelo` ,`num_serie` ,`disponible` ,`activo`) VALUES (`20202` ,`modellillo` ,`10001` ,`false` ,`true`)";
-		dba.modificar(sentencia);
-		dba.cerrarCon();*/
-		}
-		else{
-			JOptionPane.showMessageDialog(null, "Los datos son incorrectos. Compruebe los datos.");
+		if (b1 = true) {
+
+			this.iddispositivo = iddispositivo;
+			this.modelo = modelo;
+			this.numSerie = numSerie;
+		} else {
+			JOptionPane.showMessageDialog(null,
+					"Los datos son incorrectos. Compruebe los datos.");
 		}
 	}
 
-	
-	
-	
-	
-	///////////////////////////MÉTODOS//////////////////
-	
-	/* Comprobar que los datos introducidos mediante la interfaz sean correctos.
-	 * En cuyo caso, se subirán a la base de datos.
-	*/
-	
-	public Boolean validarDatos(String iddispositivo, String modelo, String numSerie){
-		
-		
-		if(iddispositivo.length() != 5){ // Longitud del id de dispositivo, que ha de ser de 5 caracteres.
-			
-			JOptionPane.showMessageDialog(null, "Error. El identificador de dispositivo ha de ser de 5 caracteres.");
+	/***************************************************************************
+	********************************MÉTODOS*************************************
+	****************************************************************************/
+	/**
+	 * Este método comprobará que los datos introducidos mediante la interfaz
+	 * sean correctos. En cuyo caso, se podrán subir a la base de datos.
+	 */
+	public Boolean validarDatos(String iddispositivo, String modelo,
+			String numSerie) {
+
+		if (iddispositivo.length() != 5) {
+			/**
+			 * Longitud del id de dispositivo, que ha de ser de 5 caracteres.
+			 **/
+
+			JOptionPane
+					.showMessageDialog(null,
+							"Error. El identificador de dispositivo ha de ser de 5 caracteres.");
 			return false;
-			
-		}else{			
-			
+
+		} else {
+
 			Boolean b = isInteger(iddispositivo);
-			
-			if(b = false ){
-				
-				JOptionPane.showMessageDialog(null, "Error. El identificador de dispositivo ha de ser numérico.");
+
+			if (b = false) {
+
+				JOptionPane
+						.showMessageDialog(null,
+								"Error. El identificador de dispositivo ha de ser numérico.");
 				return false;
-				
-			}else{
-				
-				//r = isInteger(apellidos);
-				
-				if(modelo.length() > 15){//Longitud del modelo, que ha de ser de 15 caracteres como máximo.
-					
-					JOptionPane.showMessageDialog(null, "Error. La longitud del modelo no puede superar los 15 caracteres.");
+
+			} else {
+
+				if (modelo.length() > 15) {
+					/**
+					 * Longitud del modelo, que ha de ser de 15 caracteres como
+					 * máximo.
+					 **/
+
+					JOptionPane
+							.showMessageDialog(null,
+									"Error. La longitud del modelo no puede superar los 15 caracteres.");
 					return false;
-					
-					
-				}else{
-					
-					if(numSerie.length() != 5 ){
-						
-						JOptionPane.showMessageDialog(null, "Error. El número de serie tiene que tener 5 dígitos.");
+
+				} else {
+
+					if (numSerie.length() != 5) {
+
+						JOptionPane
+								.showMessageDialog(null,
+										"Error. El número de serie tiene que tener 5 dígitos.");
 						return false;
-						
-						
-					}else{
-						
+
+					} else {
+
 						b = isInteger(numSerie);
-						
-						if(b = true){
-							
-							JOptionPane.showMessageDialog(null, "Error. El número de serie ha de ser numérico.");
+
+						if (b = false) {
+
+							JOptionPane
+									.showMessageDialog(null,
+											"Error. El número de serie ha de ser numérico.");
 							return false;
-							
-							
-						}									
-					
-						else{
-									
-									
-							return true; // Si todos los datos son correctos devuelve True.
+
+						}
+
+						else {
+
+							return true;
+							/**
+							 * Si todos los datos son correctos devuelve True.
+							 */
 						}
 					}
 				}
@@ -179,24 +149,18 @@ public class Dispositivo {
 		}
 	}
 
-	
-	
+	/**
+	 * Este método comprueba que un String sea un Número. Si lo es, devuelve
+	 * "True". Si no, devueve "False"
+	 */
+	public boolean isInteger(String cadena) {
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (Exception ex) {
+			return false;
+		}
 
-
-	// Este método comprueba que un String sea un Número.
-	
-	public boolean isInteger( String cadena )
-    {  
-       try  
-       {  
-          Integer.parseInt( cadena );  
-          return true;  
-       }  
-       catch( Exception ex )  
-       {  
-          return false;  
-       }  
-       
-    }  
+	}
 
 }
--- OpenGis/src/code/google/com/opengis/gestionDAO/DispositivoDAO.java
+++ OpenGis/src/code/google/com/opengis/gestionDAO/DispositivoDAO.java
@@ -4,7 +4,12 @@ import java.sql.*;
 
 import javax.swing.JOptionPane;
 
-// Esta clase permite insertar, modificar, y dar de baja dispositivos
+/**
+ * Esta clase permite insertar, modificar, y dar de baja dispositivos
+ * 
+ * @author Iván Serrano y Jose Alapont
+ * 
+ */
 
 public class DispositivoDAO {
 
@@ -16,53 +21,90 @@ public class DispositivoDAO {
 	private String modelo;
 	private String numSerie;
 
-	//private int disponible;
-	//private int activo;
+	/**
+	 * ***************GETTERS Y SETTERS
+	 */
+	
+	public static String getResultado() {
+		return resultado;
+	}
 
-	// //////////////////// CONSTRUCTOR ///////////////////////
+	public static void setResultado(String resultado) {
+		DispositivoDAO.resultado = resultado;
+	}
 
+	public String getIddispositivo() {
+		return iddispositivo;
+	}
+
+	public void setIddispositivo(String iddispositivo) {
+		this.iddispositivo = iddispositivo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getNumSerie() {
+		return numSerie;
+	}
+
+	public void setNumSerie(String numSerie) {
+		this.numSerie = numSerie;
+	}
+
+	
+	/**
+	 * ***************CONSTRUCTOR
+	 */
 	public DispositivoDAO(String iddispositivo, String modelo, String numSerie) {
 		super();
 		this.iddispositivo = iddispositivo;
 		this.modelo = modelo;
 		this.numSerie = numSerie;
-		//this.disponible = 1;
-		//this.activo = 1;
 	}
-	
-	//***************************************************************************//
-	//********************************MÉTODOS************************************//
-	//***************************************************************************//
-	
-	// ///////////////////////// ESTE MÉTODO COMPROBARÁ QUE NO EXISTA EL DISPOSITIVO.
-	
-	public static void comprobarDispositivo(String iddispositivo)
-			throws SQLException {
 
+	/***************************************************************************
+	 ******************************** MÉTODOS*************************************
+	 ****************************************************************************/
+
+	
+	/**
+	 * ESTE MÉTODO COMPROBARÁ QUE NO EXISTA EL DISPOSITIVO.
+	 */
+
+	public static void comprobarDispositivo(String iddispositivo)throws SQLException 
+	{
 		ConectarDBA.acceder();
-		String sentencia = "SELECT * FROM `dispositivo` WHERE `iddispositivo` LIKE '"
-				+ iddispositivo + "'";
+		String sentencia = "SELECT * FROM `dispositivo` WHERE `iddispositivo` ";
 		ResultSet rs = dba.consulta(sentencia);
 		while (rs.next()) {
 			resultado = rs.getString(1);
+			System.out.println(resultado);
 		}
-
 		System.out.println("Enviado: " + iddispositivo + " esperado: "
 				+ resultado);
 		if (resultado == null) {
 			existe = false;
 			System.out.println("El estado de existe es: " + existe);
-		} else if (resultado.equals(iddispositivo)) {
+		}
+		else if (resultado.equals(iddispositivo)) 
+		{
 			existe = true;
 			System.out.println("El estado de existe es: " + existe);
 		}
-
 		rs.close();
 	}
 
-	// ///////////////////////// ESTE MÉTODO DARÁ DE ALTA EL DISPOSITIVO INDICADO,
-	// ///////////////////////// COMPROBANDO QUE NO EXISTA.
-
+	/**
+	 * ESTE MÉTODO DARÁ DE ALTA EL DISPOSITIVO INDICADO, COMPROBANDO QUE NO
+	 * EXISTA.
+	 */
+	
 	public void altaDispositivo() throws SQLException {
 		comprobarDispositivo(this.iddispositivo);
 
@@ -72,7 +114,7 @@ public class DispositivoDAO {
 					"El ID de Dispositivo ya existe");
 
 		} else {
-
+			ConectarDBA.acceder();
 			String sentencia = "INSERT INTO `dai2opengis`.`dispositivo` (`iddispositivo` ,`modelo` ,`numSerie` ,`disponible` ,`activo`) VALUES ('"
 					+ this.iddispositivo
 					+ "', '"
@@ -90,10 +132,11 @@ public class DispositivoDAO {
 
 	}
 
-	// ///////////////////////// ESTE MÉTODO DARÁ DE BAJA EL DISPOSITIVO INDICADO,
-	// ///////////////////////// COMPROBANDO QUE YA EXISTA,
-	// ///////////////////////// Y MODIFICANDO SU CAMPO "ACTIVO" A FALSO.
-
+	/**
+	 * ESTE MÉTODO DARÁ DE BAJA EL DISPOSITIVO INDICADO, COMPROBANDO QUE YA
+	 * EXISTA, Y MODIFICANDO SU CAMPO "ACTIVO" A FALSO.
+	 */
+	
 	public void borrarDispositivo() throws SQLException {
 		comprobarDispositivo(this.iddispositivo);
 		if (existe == true) {
@@ -113,5 +156,23 @@ public class DispositivoDAO {
 		dba.cerrarCon();
 
 	}
+	public void modificarDispositivo() throws SQLException {
+		comprobarDispositivo(this.iddispositivo);
+		if (existe == true) {
 
+			String sentencia = "UPDATE dispositivo SET `modelo` = '"+this.modelo+"', `num_serie` = "+this.numSerie+"' WHERE `iddispositivo` = '"
+					+ this.iddispositivo + "'";
+			dba.modificar(sentencia);
+
+			JOptionPane.showMessageDialog(null,
+					"Dispositivo dado de baja correctamente");
+
+		} else {
+
+			JOptionPane.showMessageDialog(null, "El dispositivo no existe");
+		}
+
+		dba.cerrarCon();
+
+	}
 }
