1af5d2c0705b6247688b197433502ddc237e2555
nacasvil <nacasvil@gmail.com>
2011-11-18 18:30:43 +0000

    Clase Apero Visual.

---
+++ OpenGis/src/code/google/com/opengis/gestionVISUAL/AperoVisual.java
@@ -0,0 +1,13 @@
+package code.google.com.opengis.gestionVISUAL;
+
+import javax.swing.JInternalFrame;
+
+public class AperoVisual extends JInternalFrame {
+
+	public AperoVisual() {
+		super("Aperos",false,true,true,true);
+		System.out.println("Creado Apero Visual");
+		
+	}
+
+}
