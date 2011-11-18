75e4947fa412404236dede9cb8b59a98da0eef26
jacjose10 <jacjose10@gmail.com>
2011-11-18 17:43:24 +0000

    José Alapont ha puesto los archivos .jar que son necesarios para utilizar el JUnit. y ha credado el paquete para todas las pruebas con una clase todavía no implementada.

---
+++ OpenGis/src/jUnit/JUnitUsuarios.java
@@ -0,0 +1,13 @@
+package jUnit;
+
+//import static org.junit.Assert.*;
+
+import code.google.com.opengis.gestion.Usuarios;
+
+public class JUnitUsuarios {
+	
+/**
+ * Aquí va el código lógico de las pruebas de Usuarios.
+ */
+
+}
