bbf5dcaa5196807bdf82b460ad4f7f7e0944e5b4
Pipepito <pipepito@gmail.com>
2011-11-24 20:09:30 +0000

    Añadida la tabla a la clase Usuario Visual, faltan lso métodos de los botones.

--- OpenGis/src/code/google/com/opengis/gestionVISUAL/UsuarioVisual.java
+++ OpenGis/src/code/google/com/opengis/gestionVISUAL/UsuarioVisual.java
@@ -23,8 +23,11 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 import code.google.com.opengis.gestion.Usuarios;
 import code.google.com.opengis.gestion.ValidarLogin;
@@ -56,20 +59,34 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 	private JLabel lblFotoMod;
 	private JLabel lblDNIMod;
 	
-	private static JTextField txtDNI;
-	private static JTextField txtNombre;
-	private static JTextField txtApellidos;
-	private static JTextField txtFNac;
-	private static JTextField txtDir;
-	private static JTextField txtPob;
-	private static JTextField txtProv;
-	private static JTextField txtCp;
-	private static JTextField txtTlf;
-	private static JTextField txtEmail;
-	private static JTextField txtDNIMod;
-	private static JTextField txtCon;
-	private static JComboBox jCmbTipo;
-	private static String[] tipo = {"Administrador", "Trabajador"} ;
+	private static JTextField txtDNI = new JTextField();
+	private static JTextField txtNombre = new JTextField();
+	private static JTextField txtApellidos = new JTextField();
+	private static JTextField txtFNac = new JTextField();
+	private static JTextField txtDir = new JTextField();
+	private static JTextField txtPob = new JTextField();
+	private static JTextField txtProv = new JTextField();
+	private static JTextField txtCp = new JTextField();
+	private static JTextField txtTlf = new JTextField();
+	private static JTextField txtEmail = new JTextField();
+	private static JTextField txtDNIMod = new JTextField();
+	private static JTextField txtCon = new JTextField();
+	
+	
+	
+	
+	private static String[] tipo = {"Trabajador", "Administrador"} ;
+	private static JComboBox jCmbTipo = new JComboBox(tipo);
+	
+	private static JTextField txtBuscar = new JTextField();
+	private static String[] criterio = {"DNI", "Nombre", "Apellidos"};
+	private static JComboBox jCmbCriterio = new JComboBox(criterio);
+	
+	private static JScrollPane jScrollPaneTablaUsuarios = null;
+	private static JTable jTablaUsuarios = null;
+	static Object[] nombreColumna = {"DNI", "Nombre", "Apellidos", "Dirección", "Poblacion", "Provincia", "Cod. Postal", "Teléfono", "Email", "Tipo"};
+	static DefaultTableModel modelo = new DefaultTableModel(nombreColumna, 0);
+
 	
 	/**
 	 * Constructor de la clase UsuarioVisual. Se le pasarán los parametros necesarios para construir el alto y el ancho.
@@ -91,75 +108,144 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			panelUsuarios.setBounds(new Rectangle(0,0,(int)ii,(int)aa));
 		
 		
+			this.getJScrollPaneTablaUsuarios();
+			//cargarNuevoModificar(panelUsuarios,true);
+			cargarUsuariosPrincipal(panelUsuarios);
+		
+	}
+	
+	public static void cargarUsuariosPrincipal(Container pane){
+		JButton boton;
+		JLabel campolbl;
+		pane.setLayout(new GridBagLayout());
+		txtBuscar.setPreferredSize(new Dimension(120,20));
+		
+		//Se crean 3 constraints, uno para cada uso.
+		GridBagConstraints cText = new GridBagConstraints(); //Cajas de texto
+		GridBagConstraints cButtons = new GridBagConstraints(); //Botones
+		GridBagConstraints cLabels = new GridBagConstraints(); //Labels
+		GridBagConstraints cTabla = new GridBagConstraints();
+		
+		cLabels.insets = new Insets(8,8,8,8);  //top padding
+		
+		cLabels.weightx = 0.005;
+		cLabels.anchor = GridBagConstraints.EAST;
+		cText.anchor = GridBagConstraints.EAST;
+		cText.weightx = 0.5;
+		cText.ipadx = 100;
+		
+		
+		cText.gridx = 0;
+		cText.gridy = 0;
+		cText.gridwidth = 2;
+		txtBuscar.setText("Introduzca aquí el criterio");
+		pane.add(txtBuscar, cText);
+		cText.gridwidth = 1;
+		
+		campolbl = new JLabel("Buscar por:");
+		cLabels.gridx = 2;
+		cLabels.gridy = 0;
+		pane.add(campolbl, cLabels);
+		
+		cText.anchor = GridBagConstraints.WEST;
+		cText.gridx = 3;
+		cText.gridy = 0;
+		pane.add(jCmbCriterio, cText);
+		
+		boton = new JButton("Buscar");
+		cButtons.anchor = GridBagConstraints.WEST;
+		cButtons.insets = new Insets(0,0,0,0);
+		cButtons.gridx = 4;
+		cButtons.gridy = 0;
+		pane.add(boton, cButtons);
+		
+		
+		jTablaUsuarios.setModel(modelo);
+		cTabla.gridx = 0;
+		cTabla.gridy = 1;
+		cTabla.gridwidth = 5;
+		cTabla.gridheight = 5;
+		cTabla.fill = GridBagConstraints.HORIZONTAL;
+		pane.add(jScrollPaneTablaUsuarios, cTabla);
+		
+		
+		
+		
+		
+		
+		boton = new JButton("Nuevo");
+		cButtons.anchor = GridBagConstraints.EAST;
+		cButtons.insets = new Insets(0,0,0,0);
+		cButtons.gridx = 0;
+		cButtons.gridy = 7;
+		pane.add(boton, cButtons);
+		
+		boton = new JButton("Modificar");
+		cButtons.anchor = GridBagConstraints.WEST;
+		cButtons.insets = new Insets(0,0,0,0);
+		cButtons.gridx = 1;
+		cButtons.gridy = 7;
+		pane.add(boton, cButtons);
+		
+		boton = new JButton("Desactivar");
+		cButtons.anchor = GridBagConstraints.LINE_START;
+		cButtons.insets = new Insets(0,0,0,0);
+		cButtons.gridx = 3;
+		cButtons.gridy = 7;
+		pane.add(boton, cButtons);
+		
+			
+		
 
-			cargarNuevoModificar(panelUsuarios,true);
 		
 		
 	}
 	
-	
-	/**
-	 * Este método carga en el formulario los Objetos necesarios Ej: Cajas de Texto, Labels, etc...
-	 * 
-	 */
-	
-			
-		final static boolean shouldFill = true;
-	    final static boolean shouldWeightX = true;
-	    final static boolean RIGHT_TO_LEFT = false;
-
+/**
+ * Este método carga en el frame que le indiquemos los objetos para Crear Usuarios o Modificarlos.		
+ * @param pane Indica el contenedor de todos
+ * @param modificar Indica si vamos a modificar el Usuario (true) o vamos a crear uno nuevo (false)
+ */
 	    public static void cargarNuevoModificar(Container pane,boolean modificar) {
-	    	if (RIGHT_TO_LEFT) {
-				pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-			}
+	    	
 			JButton boton;
-			txtDNI = new JTextField();
-			txtNombre = new JTextField();
-			txtApellidos = new JTextField();
-			txtFNac = new JTextField();
-			txtDir = new JTextField();
-			txtPob = new JTextField();
-			txtProv = new JTextField();
-			txtCp = new JTextField();
-			txtTlf = new JTextField();
-			txtEmail = new JTextField();
-			txtCon = new JTextField();
-			jCmbTipo = new JComboBox(tipo);
 			JLabel campolbl;
 			pane.setLayout(new GridBagLayout());
-			
-			
+			Dimension tamañoCaja = new Dimension(100, 20);
+			txtDNI.setPreferredSize(tamañoCaja);
+			txtNombre.setPreferredSize(tamañoCaja);
+			txtApellidos.setPreferredSize(tamañoCaja);
+			txtFNac.setPreferredSize(tamañoCaja);
+			txtDir.setPreferredSize(tamañoCaja);
+			txtPob.setPreferredSize(tamañoCaja);
+			txtProv.setPreferredSize(tamañoCaja);
+			txtCp.setPreferredSize(tamañoCaja);
+			txtCon.setPreferredSize(tamañoCaja);
+			txtTlf.setPreferredSize(tamañoCaja);
+			txtEmail.setPreferredSize(tamañoCaja);
 			
 			//Se crean 3 constraints, uno para cada uso.
-			GridBagConstraints cText = new GridBagConstraints(); 
-			GridBagConstraints cButtons = new GridBagConstraints();
-			GridBagConstraints cLabels = new GridBagConstraints();
+			GridBagConstraints cText = new GridBagConstraints(); //Cajas de texto
+			GridBagConstraints cButtons = new GridBagConstraints(); //Botones
+			GridBagConstraints cLabels = new GridBagConstraints(); //Labels
 			
-			Dimension tamañoCaja = new Dimension(150, 25);
-			if (shouldFill){
-				cLabels.fill = GridBagConstraints.HORIZONTAL;
-			}
+			
+			
+						
 			cLabels.insets = new Insets(8,8,8,8);  //top padding
-			campolbl = new JLabel("DNI:");
-			if (shouldWeightX){
-				cLabels.weightx = 0.005;
-			}
 			cLabels.anchor = GridBagConstraints.EAST;
 			cText.anchor = GridBagConstraints.WEST;
-			cLabels.fill = GridBagConstraints.HORIZONTAL;
+			cText.weightx = 0.5;
+			cText.ipadx = 100;	
+			
+			campolbl = new JLabel("DNI:");
+			cLabels.weightx = 0.005;
 			cLabels.gridx = 0;
 			cLabels.gridy = 0;
 			pane.add(campolbl, cLabels);
-			
-			
-			cText.weightx = 0.5;
-			cText.ipadx = 100;
-			
-			//cText.fill = GridBagConstraints.HORIZONTAL;
-			txtDNI.setPreferredSize(tamañoCaja);
+									
 			cText.gridx = 1;
 			cText.gridy = 0;
-			txtDNI.setText("");
 			pane.add(txtDNI, cText);
 
 			
@@ -168,33 +254,31 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			cLabels.gridx = 2;
 			cLabels.gridy = 0;
 			pane.add(campolbl, cLabels);
-			txtNombre.setPreferredSize(tamañoCaja);			
+
 			cText.gridx = 3;
 			cText.gridy = 0;
 			pane.add(txtNombre, cText);
-			
-			
+				
 			
 			campolbl = new JLabel("Apellidos:");
 			cLabels.gridx = 4;
 			cLabels.gridy = 0;
 			pane.add(campolbl, cLabels);
-			
-			
-			
+						
 			cText.gridx = 5;
 			cText.gridy = 0;
 			pane.add(txtApellidos, cText);
+			
 			
 			campolbl = new JLabel("Fecha de Nacimiento:");
 			cLabels.gridx = 0;
 			cLabels.gridy = 1;
 			pane.add(campolbl, cLabels);
-			
-			
+						
 			cText.gridx = 1;
 			cText.gridy = 1;
 			pane.add(txtFNac, cText);
+			
 			
 			//Añade JLabel de Teléfono y JTextField de teléfono
 			campolbl = new JLabel("Teléfono:");
@@ -206,6 +290,7 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			cText.gridy = 1;
 			pane.add(txtTlf, cText);
 			
+			
 			campolbl = new JLabel("Dirección:");
 			cLabels.gridx = 4;
 			cLabels.gridy = 1;
@@ -214,8 +299,7 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			cText.gridx = 5;
 			cText.gridy = 1;
 			pane.add(txtDir, cText);
-
-					
+								
 			
 			campolbl = new JLabel("Población:");
 			cLabels.gridx = 0;
@@ -226,27 +310,26 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			cText.gridy = 2;
 			pane.add(txtPob, cText);
 			
-			
-			
+						
 			campolbl = new JLabel("Provincia:");
 			cLabels.gridx = 2;
 			cLabels.gridy = 2;
 			pane.add(campolbl, cLabels);
-			
-			
+						
 			cText.gridx = 3;
 			cText.gridy = 2;
 			pane.add(txtProv, cText);
+			
 			
 			campolbl = new JLabel("CP:");
 			cLabels.gridx = 4;
 			cLabels.gridy = 2;
 			pane.add(campolbl, cLabels);
-			
-			
+						
 			cText.gridx = 5;
 			cText.gridy = 2;
 			pane.add(txtCp, cText);
+			
 			
 			campolbl = new JLabel("Email:");
 			cLabels.gridx = 0;
@@ -256,9 +339,7 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			cText.gridx = 1;
 			cText.gridy = 3;
 			pane.add(txtEmail, cText);
-			
-			
-			
+									
 			
 			campolbl = new JLabel("Contraseña:");
 			cLabels.gridx = 2;
@@ -269,6 +350,7 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			cText.gridy = 3;
 			pane.add(txtCon, cText);
 			
+			
 			campolbl = new JLabel("Tipo de usuario");
 			cLabels.gridx = 4;
 			cLabels.gridy = 3;
@@ -278,29 +360,26 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			cText.gridy = 3;
 			pane.add(jCmbTipo,cText);
 			
-			if (modificar != true){
-				
 			
-			boton = new JButton("Guardar");
-			cButtons.fill = 0;
-			cButtons.anchor = GridBagConstraints.PAGE_END; //bottom of space
-			cButtons.insets = new Insets(15,0,0,0);  //top padding
-			cButtons.gridx = 0;
-			cButtons.gridy = 5;
-			boton.addActionListener(new java.awt.event.ActionListener() {
-				public void actionPerformed(java.awt.event.ActionEvent e) {			
+			if (modificar != true){
+				boton = new JButton("Guardar");
+				cButtons.fill = 0;
+				cButtons.anchor = GridBagConstraints.PAGE_END; //bottom of space
+				cButtons.insets = new Insets(15,0,0,0);  //top padding
+				cButtons.gridx = 0;
+				cButtons.gridy = 5;
+				boton.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent e) {			
 									
-					Usuarios u = new Usuarios(txtDNI.getText(), txtNombre.getText(), txtApellidos.getText(), txtTlf.getText(), txtDir.getText(), txtPob.getText(),txtProv.getText(), txtCp.getText(), txtFNac.getText(), txtCon.getText(), jCmbTipo.getSelectedItem().toString().toLowerCase(), txtEmail.getText());
-					u.validarDatos();
-					System.out.println(jCmbTipo.getSelectedItem().toString());
-					if (u.getValido()) {
-						u.crearUsuario();
+						Usuarios u = new Usuarios(txtDNI.getText(), txtNombre.getText(), txtApellidos.getText(), txtTlf.getText(), txtDir.getText(), txtPob.getText(),txtProv.getText(), txtCp.getText(), txtFNac.getText(), txtCon.getText(), jCmbTipo.getSelectedItem().toString().toLowerCase(), txtEmail.getText());
+						u.validarDatos();
+						System.out.println(jCmbTipo.getSelectedItem().toString());
+						if (u.getValido()) {
+							u.crearUsuario();
+						}					
 					}
-					
-					
-				}
-			});
-			pane.add(boton, cButtons);
+				});
+				pane.add(boton, cButtons);
 			}else{
 				boton = new JButton("Modificar");
 				cButtons.fill = 0;
@@ -357,6 +436,14 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 			
 
 	}
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 	/**
 	 *  Este metodo hace que cuando clickes el boton de modificar te lleve a la interface de modificar usuario
 	 */
@@ -421,6 +508,32 @@ public class UsuarioVisual extends JInternalFrame implements ActionListener {
 	}
 	public void setTipo(String[] tipo) {
 		this.tipo = tipo;
+	}
+	
+	private JScrollPane getJScrollPaneTablaUsuarios() {
+		if (jScrollPaneTablaUsuarios == null) {
+			jScrollPaneTablaUsuarios = new JScrollPane();
+			//jScrollPaneTablaUsuarios.setBounds(new Rectangle(9, 73, 638, 117));
+			jScrollPaneTablaUsuarios.setViewportView(getJTablaUsuarios());
+		}
+		return jScrollPaneTablaUsuarios;
+	}
+
+	/**
+	 * This method initializes jTableAsignaturas	
+	 * 	
+	 * @return javax.swing.JTable	
+	 */
+	private JTable getJTablaUsuarios() {
+		
+			
+			jTablaUsuarios = new JTable();
+			jTablaUsuarios.setModel(modelo);
+					
+			
+			
+		
+		return jTablaUsuarios;
 	}
 
 }
