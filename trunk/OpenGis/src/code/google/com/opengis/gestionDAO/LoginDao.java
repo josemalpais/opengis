e7405ba6e82aefb10bf06f800da175a126a3aeab
denycow <denycow@1bd2956d-1e62-b8c4-f405-d16e971eae1f>
2011-11-28 18:32:19 +0000

    ya no entra si el usuario esta desactivado

--- trunk/OpenGis/src/code/google/com/opengis/gestionDAO/LoginDao.java
+++ trunk/OpenGis/src/code/google/com/opengis/gestionDAO/LoginDao.java
@@ -1,123 +1,127 @@
 package code.google.com.opengis.gestionDAO;
 
-	import java.sql.ResultSet;
-	import java.sql.SQLException;
-	
-	public class LoginDao {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDao {
 	String user;
 	String password;
 	char tipodato;
-	 String lol;
-	
+	String lol;
+
 	/***
-	 *CONSTRUCTOR
+	 * CONSTRUCTOR
 	 ***/
-	public LoginDao(String user, String password){
-		this.user= user;
+	public LoginDao(String user, String password) {
+		this.user = user;
 		this.password = password;
-		
+
 	}
-	public LoginDao(String user){
-		this.user= user;
+
+	public LoginDao(String user) {
+		this.user = user;
 		this.password = null;
-		
+
 	}
+
 	/***
-	 *Getters & setters
+	 * Getters & setters
 	 ***/
-		public String getUser() {
+	public String getUser() {
 		return user;
 	}
-	
+
 	public void setUser(String user) {
 		this.user = user;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public char getTipodato() {
 		return tipodato;
 	}
-	
+
 	public void setTipodato(char tipodato) {
 		this.tipodato = tipodato;
 	}
-	
-	
+
 	/***
-	 *Metodo que introduce el usuario y contraseña a la base de datos para comparar y devuelve un
-	 *char con el tipo de usuario que tiene que ser
+	 * Metodo que introduce el usuario y contraseña a la base de datos para
+	 * comparar y devuelve un char con el tipo de usuario que tiene que ser
 	 ***/
-		public char datosDba() throws SQLException{
+	public char datosDba() throws SQLException {
 		ConectarDBA dba = new ConectarDBA();
-	    dba.acceder();		     
-	    
-	    
-	     String sentencia = "SELECT dni, password, tipo FROM usuario WHERE dni LIKE '"+this.user+"'AND password LIKE '"+this.password+"'";
-	     ResultSet rs = dba.consulta(sentencia);
-	     
-	     while (rs.next()){
-	    	 String tipo = rs.getString(3);
-		      tipodato = tipo.charAt(0);
-		      } 
-		      rs.close();
-		      dba.cerrarCon();
-	     return tipodato;
-		}
+		dba.acceder();
+
+		String sentencia = "SELECT dni, password, tipo, activo FROM usuario WHERE dni LIKE '"
+				+ this.user + "'AND password LIKE '" + this.password + "' AND `activo` = '0'";
+		ResultSet rs = dba.consulta(sentencia);
 		
-		public String email() throws SQLException{
-			ConectarDBA dba = new ConectarDBA();
-		    dba.acceder();		     
-		    
-		    
-		     String sentencia = "SELECT usuario.email FROM dai2opengis.usuario WHERE dni LIKE '"+this.user+"'";
-		     ResultSet rs = dba.consulta(sentencia);
-		    
-		     while (rs.next()){
-		    	  lol = rs.getString(1);
-		    	 }
-		     return lol;
+		while (rs.next()) {
+			String tipo = rs.getString(3);
+			tipodato = tipo.charAt(0);
 		}
-		public String pass() throws SQLException{
-			ConectarDBA dba = new ConectarDBA();
-		    dba.acceder();		     
-		    
-		    
-		     String sentencia = "SELECT usuario.password FROM dai2opengis.usuario WHERE dni LIKE '"+this.user+"'";
-		     ResultSet rs = dba.consulta(sentencia);
-		     while (rs.next()){
-		    	  lol = rs.getString(1);
-		    	 }
-		     return lol;
-		}
-		public String nombre() throws SQLException{
-			ConectarDBA dba = new ConectarDBA();
-		    dba.acceder();		     
-		    
-		    
-		     String sentencia = "SELECT usuario.nombre FROM dai2opengis.usuario WHERE dni LIKE '"+this.user+"'";
-		     ResultSet rs = dba.consulta(sentencia);
-		     while (rs.next()){
-		    	  lol = rs.getString(1);
-		    	 }
-		     return lol;
-		}
-		public String apellido() throws SQLException{
-			ConectarDBA dba = new ConectarDBA();
-		    dba.acceder();		     
-		    
-		    
-		     String sentencia = "SELECT usuario.apellidos FROM dai2opengis.usuario WHERE dni LIKE '"+this.user+"'";
-		     ResultSet rs = dba.consulta(sentencia);
-		     while (rs.next()){
-		    	  lol = rs.getString(1);
-		    	 }
-		     return lol;}
+		rs.close();
+		dba.cerrarCon();
+		return tipodato;
 	}
-	
+
+	public String email() throws SQLException {
+		ConectarDBA dba = new ConectarDBA();
+		dba.acceder();
+
+		String sentencia = "SELECT usuario.email FROM dai2opengis.usuario WHERE dni LIKE '"
+				+ this.user + "'";
+		ResultSet rs = dba.consulta(sentencia);
+
+		while (rs.next()) {
+			lol = rs.getString(1);
+		}
+		return lol;
+	}
+
+	public String pass() throws SQLException {
+		ConectarDBA dba = new ConectarDBA();
+		dba.acceder();
+
+		String sentencia = "SELECT usuario.password FROM dai2opengis.usuario WHERE dni LIKE '"
+				+ this.user + "'";
+		ResultSet rs = dba.consulta(sentencia);
+		while (rs.next()) {
+			lol = rs.getString(1);
+		}
+		return lol;
+	}
+
+	public String nombre() throws SQLException {
+		ConectarDBA dba = new ConectarDBA();
+		dba.acceder();
+
+		String sentencia = "SELECT usuario.nombre FROM dai2opengis.usuario WHERE dni LIKE '"
+				+ this.user + "'";
+		ResultSet rs = dba.consulta(sentencia);
+		while (rs.next()) {
+			lol = rs.getString(1);
+		}
+		return lol;
+	}
+
+	public String apellido() throws SQLException {
+		ConectarDBA dba = new ConectarDBA();
+		dba.acceder();
+
+		String sentencia = "SELECT usuario.apellidos FROM dai2opengis.usuario WHERE dni LIKE '"
+				+ this.user + "'";
+		ResultSet rs = dba.consulta(sentencia);
+		while (rs.next()) {
+			lol = rs.getString(1);
+		}
+		return lol;
+	}
+}
