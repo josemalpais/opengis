fe477981d21fe19495c7c77844310ceb3bea3294
ivsepa <ivsepa@gmail.com>
2011-12-02 19:38:55 +0000

    Creada la clase "Alquiler" , en proceso de implementar validaciones, y falta la creación de métodos de alquiler y devolución

---
+++ OpenGis/src/code/google/com/opengis/gestion/Alquiler.java
@@ -0,0 +1,212 @@
+package code.google.com.opengis.gestion;
+
+import javax.swing.JOptionPane;
+
+public class Alquiler {
+	private String iddispositivo;
+	private String dni_usuario;
+	private String fecha_alquiler;
+	private String fecha_devolucion;
+	private String periodo;
+	
+	
+	/**
+	 * 
+	 * @return Este método nos devuelve el Identificador de dispositivo
+	 */
+	public String getIddispositivo() {
+		return iddispositivo;
+	}
+	/**
+	 * Con este método cambiamos Identificador del dispositivo del alquiler
+	 * @param iddispositivo  Le pasamos el nuevo Identificador
+	 */
+	public void setIddispositivo(String iddispositivo) {
+		this.iddispositivo = iddispositivo;
+	}
+	/**
+	 * 
+	 * @return Este método nos devuelve el DNI del usuario que realiza este alquiler
+	 */
+	public String getDni_usuario() {
+		return dni_usuario;
+	}
+	/**
+	 * Con este método cambiamos el DNI del usuario que realiza este alquiler
+	 * @param dni_usuario Le pasamos el nuevo DNI
+	 */
+	public void setDni_usuario(String dni_usuario) {
+		this.dni_usuario = dni_usuario;
+	}
+	/**
+	 * Este método nos devuelve la fecha en la que se inicia este alquiler
+	 * @return
+	 */
+	public String getFecha_alquiler() {
+		return fecha_alquiler;
+	}
+	/**
+	 * Con este método cambiamos la fecha en la que se inicia este alquiler
+	 * @param fecha_alquiler Le pasamos la nueva fecha de alquiler
+	 */
+	public void setFecha_alquiler(String fecha_alquiler) {
+		this.fecha_alquiler = fecha_alquiler;
+	}
+	/**
+	 * Este método nos devuelve la fecha en la que se finaliza este alquiler
+	 * @return
+	 */
+	public String getFecha_devolucion() {
+		return fecha_devolucion;
+	}
+	/**
+	 * Con este método cambiamos la fecha en la que se finaliza este alquiler
+	 * @param fecha_devolucion Le pasamos la nueva fecha de devolución
+	 */
+	public void setFecha_devolucion(String fecha_devolucion) {
+		this.fecha_devolucion = fecha_devolucion;
+	}
+	/**
+	 * Este método nos devuelve el período de este alquiler
+	 * @return
+	 */
+	public String getPeriodo() {
+		return periodo;
+	}
+	/**
+	 * Con este método cambiamos el período de este alquiler
+	 * @param periodo Le pasamos el nuevo período de alquiler
+	 */
+	public void setPeriodo(String periodo) {
+		this.periodo = periodo;
+	}
+	public Alquiler(String iddispositivo, String dni_usuario,
+			String fecha_alquiler, String fecha_devolucion, String periodo) {
+		this.iddispositivo = iddispositivo;
+		this.dni_usuario = dni_usuario;
+		this.fecha_alquiler = fecha_alquiler;
+		this.fecha_devolucion = fecha_devolucion;
+		this.periodo = periodo;
+	}
+	
+	public String calcularDNI(String dni) {
+		int pletra;
+		String aux = "";
+		String[] arrayLetra = { "T", "R", "W", "A", "G", "M", "Y", "F", "P",
+				"D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C",
+				"K", "E", "T" };
+		for (int x = 0; x < 8; x++) {// este for nos cojera los primeros
+										// carácteres y los guardara en el
+										// string aux
+			aux = aux + dni.charAt(x);
+		}
+		pletra = Integer.parseInt(aux);
+		pletra = pletra % 23;
+		aux = aux + arrayLetra[pletra];
+
+		return dni;
+	}
+
+	/**
+	 * Clase que valida el DNI, y lo coloca en formato 00000000L
+	 * 
+	 * @param dni
+	 * @return boolean que nos determinará si el dni introducido es verdadero o
+	 *         falso.
+	 */
+	public boolean validarDni(String dni) {
+		int pletra;
+		String aux = "";
+		String aux2 = "";
+		String[] arrayLetra = { "T", "R", "W", "A", "G", "M", "Y", "F", "P",
+				"D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C",
+				"K", "E", "T" };
+
+		for (int x = 0; x < 8; x++) {// este for nos cojera los 8 primeros
+										// carácteres y los guardará en el
+										// string aux
+			aux = aux + dni.charAt(x);
+		}
+		try {
+			pletra = Integer.parseInt(aux); // si no fueran enteros saldriamos
+											// del metodo con un false
+		} catch (NumberFormatException ex) {
+			JOptionPane.showMessageDialog(null,
+					"LOS 8 PRIMEROS DIGITOS HAN DE SER ENTEROS");
+			return false;
+		}
+		pletra = pletra % 23;
+		aux2 = dni.charAt(dni.length() - 1) + "";
+
+		if (arrayLetra[pletra].equalsIgnoreCase(aux2)) {
+			String dni1 = aux + arrayLetra[pletra];// lo colocamos en formato de 9
+												   // carácteres
+			return true;
+		} else {
+			JOptionPane
+					.showMessageDialog(null,
+							"EL NUMERO QUE HA INTRODUCIDO NO SE CORRESPONDE CON LA LETRA");
+			valido = false;
+			return false;
+		}
+	}
+	
+public static boolean validarDatos(String iddispositivo, String dni_usuario,
+		String fecha_alquiler, String fecha_devolucion, String periodo){
+	boolean b = false;
+	
+	//Compruebo que el ID de dispositivo sea numérico
+	b = isInteger(iddispositivo);
+	
+	if (b == false) { 
+		JOptionPane
+				.showMessageDialog(null,
+						"Error. El ID de dispositivo ha de ser numérico.");
+		return false;
+
+	} else {
+
+		if ((numSerie.length() < 10) || (numSerie.length() > 30)) {
+
+			JOptionPane
+					.showMessageDialog(null,
+							"Error. El número de serie tiene que tener entre 10 y 30 dígitos.");
+			return false;
+
+		} else {
+			try {
+				numSerie = numSerie.toString();
+				b = true;
+			} catch (Exception e) {
+				b = false;
+			}
+
+			if (b = false) {
+
+				JOptionPane
+						.showMessageDialog(null,
+								"Error. El número de serie es incorrecto. Introduce datos correctos.");
+				return false;
+
+			}
+
+			else {
+
+				return true;
+				/**
+				 * Si todos los datos son correctos devuelve True.
+				 */
+			}
+		}
+	}
+}
+public static boolean isInteger(String cadena) {
+	try {
+		Integer.parseInt(cadena);
+		return true;
+	} catch (Exception ex) {
+		return false;
+	}
+
+}
+}
