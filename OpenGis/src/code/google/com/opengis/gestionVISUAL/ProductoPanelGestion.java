e23524dc108504311d7a35e5146f7067002219fc
jugade92 <jugade92@gmail.com>
2011-12-06 15:33:08 +0000

    Modificación del PaneldeGestión

--- OpenGis/src/code/google/com/opengis/gestionVISUAL/ProductoPanelGestion.java
+++ OpenGis/src/code/google/com/opengis/gestionVISUAL/ProductoPanelGestion.java
@@ -1,295 +1,219 @@
 package code.google.com.opengis.gestionVISUAL;
 
+import java.awt.GridBagLayout;
 import javax.swing.JPanel;
-
+import java.awt.Dimension;
 import javax.swing.JLabel;
 import java.awt.Rectangle;
-
-import javax.swing.Icon;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
-import javax.swing.JCheckBox;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-
-import code.google.com.opengis.gestion.Producto;
-import code.google.com.opengis.gestionDAO.ConectarDBA;
-import code.google.com.opengis.gestionDAO.Idioma;
-
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class ProductoPanelGestion extends JPanel {
-	
-	 private JButton bGuardar;
-     private JButton bRestablecer;
-     
-     private static JTextField txtIdprod;
-     private static JTextField txtNombre;
-     private static JTextField txtDosis;
-     private static JTextField txtDni;
-     private JComboBox comboTipo;
-     private static String[] tipoproducto = {"Líquido", "Granulado", "Polvo"};
-     private JCheckBox chkActivo;
-     private JTextArea txtareaDescripcion;
-     
-     private JLabel lblIdprod;
- 	 private JLabel lblNombre;
- 	 private JLabel lblDescripcion;
-     private JLabel lblTipo;
- 	 private JLabel lblDosis;
- 	 private JLabel lblDni;
- 	 private JLabel lblActivo;
- 	 private JLabel lblObligtorios;
- 	 
- 	private String accion;
- 	 
- 	private String idprod;
-	private String nombre;
-	private String descripcion;
-	private String tipo;
-	private String dosis;
-	private String dni;
- 	 
-public ProductoPanelGestion (String accion, String idprod, String nombre, String descripcion, String tipo, String dosis, String dni){
-	super();
-	this.idprod=idprod;
-	this.nombre=nombre;
-	this.descripcion=descripcion;
-	this.tipo =tipo;
-	this.dosis=dosis;
-	this.dni=dni;
-	this.accion = accion;
-	initialize();
-}
 
-public ProductoPanelGestion(String accion){
+	private static final long serialVersionUID = 1L;
+	private JLabel lblidprod = null;
+	private JLabel lblNombre = null;
+	private JLabel lblTipo = null;
+	private JLabel lblDNI = null;
+	private JLabel lblDosis = null;
+	private JLabel lblDescripcion = null;
+	private JTextField txtID = null;
+	private JTextField txtNombreProd = null;
+	private JTextField txtDNI = null;
+	private JTextField txtDosis = null;
+	private JTextArea txtDescripcion = null;
+	private JLabel lblMedida = null;
+	private JComboBox comboTipo = null;
+	private JButton bGuardar = null;
+	private JButton bLimpiar = null;
+	private JLabel lblObligatorios = null;
 	
-	super();
-	this.accion = accion;
+	String accion;
 	
-	initialize();
-	
-}
-private void initialize() {
-
-	lblObligtorios = new JLabel();
-	lblObligtorios.setBounds(new Rectangle(434, 334, 238, 25));
-	lblObligtorios.setFont(new Font(Idioma.getString("Dialog"), Font.ITALIC, 12)); //$NON-NLS-1$
-	lblObligtorios.setText(Idioma.getString("etAllFields")); //$NON-NLS-1$
-
-	lblDescripcion = new JLabel();
-	lblDescripcion.setBounds(new Rectangle(274, 192, 88, 30));
-	lblDescripcion.setText("Descripcion"); //$NON-NLS-1$
-	lblDni = new JLabel();
-	lblDni.setBounds(new Rectangle(44, 192, 88, 30));
-	lblDni.setText("Dni"); //$NON-NLS-1$
-	lblDosis = new JLabel();
-	lblDosis.setBounds(new Rectangle(274, 137, 88, 30));
-	lblDosis.setText("Dosis"); //$NON-NLS-1$
-	lblTipo = new JLabel();
-	lblTipo.setBounds(new Rectangle(42, 137, 88, 30));
-	lblTipo.setText("Tipo"); //$NON-NLS-1$
-	lblIdprod = new JLabel();
-	lblIdprod.setBounds(new Rectangle(42, 31, 88, 30));
-	lblIdprod.setText("ID producto");
-	lblNombre = new JLabel();
-	lblNombre.setBounds(new Rectangle(42, 84, 88, 30));
-	lblNombre.setText("Nombre producto"); //$NON-NLS-1$
-	this.setSize(782, 388);
-	this.setLayout(null);
-	
-	this.add(lblIdprod, null);
-	this.add(lblNombre, null);
-	this.add(lblTipo, null);
-	this.add(lblDosis, null);
-	this.add(lblDni, null);
-	this.add(lblActivo, null);
-	this.add(lblDescripcion, null);
-	this.add(lblObligtorios, null);
-	
-	this.add(getBGuardar(), null);
-	this.add(getBRestablecer(), null);
-	
-	this.add(gettxtIdprod(), null);
-	this.add(gettxtNombre(), null);
-	this.add(gettxtDosis(), null);
-	this.add(gettxtDni(), null);
-	this.add(gettxtareaDescripcion(), null);
-	this.add(getcomboTipo(), null);
-	
-}
-private JButton getBGuardar() {
-	if (bGuardar == null) {
-		bGuardar = new JButton();
-		bGuardar.setBounds(new Rectangle(46, 314, 53, 45));
-		bGuardar.setIcon(new ImageIcon(getClass().getResource("/recursosVisuales/Guardar.png"))); //$NON-NLS-1$
-	//	bGuardar.setToolTipText(Idioma.getString("etSaveNewUser")); //$NON-NLS-1$
-		bGuardar.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				   int variableint;
-					if (chkActivo.isSelected()){
-                       	variableint=0;
-                       }else{
-                       	variableint=1;
-                       }
-                       Producto p = new Producto(Integer.parseInt(txtIdprod.getText()), txtNombre.getText(), txtareaDescripcion.getText(), comboTipo.getSelectedItem().toString(), txtDosis.getText(), txtDni.getText(), variableint);        
-                       p.validarDatos();
-                       if (p.getCorrecto()) {
-							p.crearProducto();
-							 try {	
-					                ResultSet rs = ConectarDBA.consulta("SELECT MAX(idprod) FROM producto");
-					                Integer idprod; 
-					                rs.next();
-					                idprod = rs.getInt(1); 
-					                if(idprod==null){	 
-					               	 idprod = 0;
-					                }
-					                txtIdprod.setText(idprod+1+"");
-							 } catch (SQLException e2) {
-					                System.out.println(e2.getMessage());
-							 }
-                       }
-                   }
-		});
-	}
-	return bGuardar;
-}
-
-private JButton getBRestablecer() {
-	if (bRestablecer == null) {
-		bRestablecer = new JButton();
-		bRestablecer.setBounds(new Rectangle(122, 314, 53, 45));
-		bRestablecer.setIcon(new ImageIcon(getClass().getResource("/recursosVisuales/Limpiar.png"))); 
-		bRestablecer.setToolTipText(Idioma.getString("etCleanFields")); 
-		bRestablecer.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				
-				txtNombre.setText(""); //$NON-NLS-1$
-				txtDni.setText(""); //$NON-NLS-1$
-				txtDosis.setText(""); //$NON-NLS-1$
-				txtareaDescripcion.setText(""); //$NON-NLS-1$
-				chkActivo.setSelected(false); //$NON-NLS-1$
-				comboTipo.removeAllItems();
-				try { 
-	           	 	ConectarDBA.acceder();
-	                ResultSet rs = ConectarDBA.consulta("SELECT MAX(idprod) FROM producto");
-	                Integer idprod;
-	                
-	                rs.next();
-	                idprod = rs.getInt(1);
-	                
-	                if(idprod==null){
-	               	 
-	               	 idprod = 0;
-	               	 
-	                }
-	                txtIdprod.setText(idprod+1+"");
-	            } catch (SQLException e2) {
-	                System.out.println(e2.getMessage());
-	            }
-			}
-		});
-	}
-	return bRestablecer;
-}
-
-
-
-private JTextField gettxtIdprod() {
-	if (txtIdprod == null) {
-		txtIdprod = new JTextField(idprod);
-		txtIdprod.setBounds(new Rectangle(123, 33, 143, 27));
+	/**
+	 * This is the default constructor
+	 */
+	public ProductoPanelGestion(String accion) {
+		super();
 		
-		if(accion=="modificar"){ 
-			
-			txtIdprod.setEnabled(false);
+		this.accion = accion;
+		
+		initialize();
+	}
+
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		lblObligatorios = new JLabel();
+		lblObligatorios.setBounds(new Rectangle(359, 305, 227, 20));
+		lblObligatorios.setFont(new Font("Dialog", Font.ITALIC, 12));
+		lblObligatorios.setText("(*) Todos los campos son obligatorios");
+		lblMedida = new JLabel();
+		lblMedida.setBounds(new Rectangle(372, 136, 44, 25));
+		lblMedida.setFont(new Font("Dialog", Font.PLAIN, 12));
+		lblMedida.setText("(l/ha)");
+		lblDescripcion = new JLabel();
+		lblDescripcion.setBounds(new Rectangle(328, 180, 112, 25));
+		lblDescripcion.setText("Descripción:");
+		lblDosis = new JLabel();
+		lblDosis.setBounds(new Rectangle(328, 136, 44, 25));
+		lblDosis.setText("Dosis:");
+		lblDNI = new JLabel();
+		lblDNI.setBounds(new Rectangle(47, 180, 112, 25));
+		lblDNI.setText("DNI");
+		lblTipo = new JLabel();
+		lblTipo.setBounds(new Rectangle(47, 136, 112, 25));
+		lblTipo.setText("Tipo:");
+		lblNombre = new JLabel();
+		lblNombre.setBounds(new Rectangle(47, 91, 112, 25));
+		lblNombre.setText("Nombre producto:");
+		lblidprod = new JLabel();
+		lblidprod.setBounds(new Rectangle(47, 48, 112, 25));
+		lblidprod.setText("ID producto");
+		this.setSize(774, 357);
+		this.setLayout(null);
+		this.add(lblidprod, null);
+		this.add(lblNombre, null);
+		this.add(lblTipo, null);
+		this.add(lblDNI, null);
+		this.add(lblDosis, null);
+		this.add(lblDescripcion, null);
+		this.add(getTxtID(), null);
+		this.add(getTxtNombreProd(), null);
+		this.add(getTxtDNI(), null);
+		this.add(getTxtDosis(), null);
+		this.add(getTxtDescripcion(), null);
+		this.add(lblMedida, null);
+		this.add(getComboTipo(), null);
+		this.add(getBGuardar(), null);
+		this.add(getBLimpiar(), null);
+		this.add(lblObligatorios, null);
+	}
+
+	/**
+	 * This method initializes txtID	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getTxtID() {
+		if (txtID == null) {
+			txtID = new JTextField();
+			txtID.setBounds(new Rectangle(157, 48, 149, 24));
 		}
-		
+		return txtID;
 	}
-	return txtIdprod;
-}
-private JTextField gettxtNombre() {
-	if (txtNombre == null) {
-		txtNombre = new JTextField(nombre);
-		txtNombre.setBounds(new Rectangle(123, 86, 143, 27));
+
+	/**
+	 * This method initializes txtNombreProd	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getTxtNombreProd() {
+		if (txtNombreProd == null) {
+			txtNombreProd = new JTextField();
+			txtNombreProd.setBounds(new Rectangle(160, 91, 149, 24));
+		}
+		return txtNombreProd;
 	}
-	return txtNombre;
-}
 
-private JComboBox getcomboTipo() {
-	if (comboTipo == null) {
-		comboTipo = new JComboBox(tipoproducto);
-		comboTipo.setBounds(new Rectangle(123, 139, 143, 27));
-		
-		//comboTipo.addItem(Idioma.getString("etAdmin")); //$NON-NLS-1$
-		//comboTipo.addItem(Idioma.getString("etWorker")); //$NON-NLS-1$
-		comboTipo.addActionListener(new ActionListener(){
-        	public void actionPerformed (ActionEvent e){
-        		int selIndex = comboTipo.getSelectedIndex();
-        			switch (selIndex){
-        			case 0:
-        				lblDosis.setText("Dosis (l/ha):");
-        			break;
-        			case 1:
-        				lblDosis.setText("Dosis (gr/ha):");
-        			break;
-        			case 2:
-        				lblDosis.setText("Dosis (kg/ha)");
-        			break;
-        			}
-        	}
-        });
-		
+	/**
+	 * This method initializes txtDNI	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getTxtDNI() {
+		if (txtDNI == null) {
+			txtDNI = new JTextField();
+			txtDNI.setBounds(new Rectangle(160, 180, 149, 24));
+		}
+		return txtDNI;
 	}
-	return comboTipo;
-}
-private JTextField gettxtDosis() {
-	if (txtDosis == null) {
-		txtDosis = new JTextField(dosis);
-		txtDosis.setBounds(new Rectangle(358, 139, 115, 27));
+
+	/**
+	 * This method initializes txtDosis	
+	 * 	
+	 * @return javax.swing.JTextField	
+	 */
+	private JTextField getTxtDosis() {
+		if (txtDosis == null) {
+			txtDosis = new JTextField();
+			txtDosis.setBounds(new Rectangle(420, 136, 149, 24));
+		}
+		return txtDosis;
 	}
-	return txtDosis;
-}
 
-private JTextField gettxtDni() {
-	if (txtDni == null) {
-		txtDni = new JTextField(dni);
-		txtDni.setBounds(new Rectangle(123, 194, 143, 27));
+	/**
+	 * This method initializes txtDescripcion	
+	 * 	
+	 * @return javax.swing.JTextArea	
+	 */
+	private JTextArea getTxtDescripcion() {
+		if (txtDescripcion == null) {
+			txtDescripcion = new JTextArea();
+			txtDescripcion.setBounds(new Rectangle(422, 180, 161, 100));
+		}
+		return txtDescripcion;
 	}
-	return txtDni;
-}
-private JTextArea gettxtareaDescripcion() {
-	if (txtareaDescripcion == null) {
-		txtareaDescripcion = new JTextArea(descripcion);
-		txtareaDescripcion.setBounds(new Rectangle(358, 194, 115, 27));
+
+	/**
+	 * This method initializes comboTipo	
+	 * 	
+	 * @return javax.swing.JComboBox	
+	 */
+	private JComboBox getComboTipo() {
+		if (comboTipo == null) {
+			comboTipo = new JComboBox();
+			comboTipo.setBounds(new Rectangle(162, 136, 149, 24));
+			comboTipo.addItem("Líquido");
+			comboTipo.addItem("Granulado");
+			comboTipo.addItem("Polvo");
+		}
+		return comboTipo;
 	}
-	return txtareaDescripcion;
-}
 
-}
+	/**
+	 * This method initializes bGuardar	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getBGuardar() {
+		if (bGuardar == null) {
+			bGuardar = new JButton();
+			bGuardar.setBounds(new Rectangle(48, 283, 53, 45));
+			bGuardar.setIcon(new ImageIcon(getClass().getResource("/recursosVisuales/Guardar.png")));
+			bGuardar.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+				}
+			});
+		}
+		return bGuardar;
+	}
 
+	/**
+	 * This method initializes bLimpiar	
+	 * 	
+	 * @return javax.swing.JButton	
+	 */
+	private JButton getBLimpiar() {
+		if (bLimpiar == null) {
+			bLimpiar = new JButton();
+			bLimpiar.setBounds(new Rectangle(149, 283, 53, 45));
+			bLimpiar.setIcon(new ImageIcon(getClass().getResource("/recursosVisuales/Limpiar.png")));
+			bLimpiar.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+				}
+			});
+		}
+		return bLimpiar;
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}  //  @jve:decl-index=0:visual-constraint="10,10"
