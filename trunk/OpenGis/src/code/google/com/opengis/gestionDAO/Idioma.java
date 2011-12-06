1178ffed9310ea9eee0976ae707663f758bf9ce5
jugade92@gmail.com <jugade92@gmail.com@1bd2956d-1e62-b8c4-f405-d16e971eae1f>
2011-12-06 18:59:23 +0000

    YA CAMBIA EL IDIOMA!!!

--- trunk/OpenGis/src/code/google/com/opengis/gestionDAO/Idioma.java
+++ trunk/OpenGis/src/code/google/com/opengis/gestionDAO/Idioma.java
@@ -4,16 +4,22 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Idioma {
-	private static final String BUNDLE_NAME = "resources.lang"; //$NON-NLS-1$
+	
+	
+	private static String BUNDLE_NAME;
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME);
-
-	private Idioma() {
+	public Idioma(String fichero) {
+		
+		BUNDLE_NAME = fichero;
+		System.out.println(fichero);
+		
 	}
+
 
 	public static String getString(String key) {
 		try {
+			
+			ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 			return RESOURCE_BUNDLE.getString(key);
 		} catch (MissingResourceException e) {
 			return '!' + key + '!';
