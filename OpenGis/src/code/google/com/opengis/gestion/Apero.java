14098b8439819bb2c38669c795bc95ce2d5aec4a
eduardo.moret.morales <eduardo.moret.morales@gmail.com>
2011-12-21 17:32:39 +0000

    Mas etiquetas

--- OpenGis/src/code/google/com/opengis/gestion/Apero.java
+++ OpenGis/src/code/google/com/opengis/gestion/Apero.java
@@ -2,6 +2,8 @@ package code.google.com.opengis.gestion;
 
 import javax.swing.JOptionPane;
 
+import code.google.com.opengis.gestionDAO.Idioma;
+
 /**
  * @author kAStRo!
  *Esta clase nos permite validar los datos de un Apero.
@@ -43,11 +45,11 @@ public class Apero {
 	}
 
 	public void setIdApero(int idApero) {
-		String str= idApero+"";
+		String str= idApero+""; //$NON-NLS-1$
 		if((str.length()>1)&&(str.length()<9)){ //comprobamos que idapero tenga entre 1 y 8 digitos
 			this.idApero = idApero;
 		}else{
-			JOptionPane.showMessageDialog(null, "El ID del Apero debe tener entre 1 y 8 dígitos");
+			JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementIdBetween1And8")); //$NON-NLS-1$
 		}
 	}
 	
@@ -59,7 +61,7 @@ public class Apero {
 	public void setNomApero(String nomApero) {
 		Boolean nm = isInteger(nomApero);
 		if(nm.equals(true) || nomApero.length() < 2 || nomApero.length()>20){
-			JOptionPane.showMessageDialog(null, "Error. El nombre del apero no puede ser numerico ni estar vacío");			
+			JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementNameNumericNorEmpty"));			 //$NON-NLS-1$
 		}else{
 			this.nomApero = nomApero;
 		}
@@ -72,7 +74,7 @@ public class Apero {
 
 	public void setTamApero(int tamApero) {
 		if(this.tamApero<=0||this.tamApero>15){
-			JOptionPane.showMessageDialog(null, "Error. El tamaño del apero no puede ser alfabético o menor que 0");		
+			JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementSizeCharsNorNegative"));		 //$NON-NLS-1$
 		}else{
 			this.tamApero = tamApero;
 		}
@@ -85,7 +87,7 @@ public class Apero {
 
 	public void setDescApero(String descApero) {
 		if(descApero.length()>30||descApero.length()<=0){
-			JOptionPane.showMessageDialog(null, "La descripcion del apero debe constar de entre 0 y 30 carácteres alfanuméricos");
+			JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementWrongDesc")); //$NON-NLS-1$
 		}else{
 			this.descApero = descApero;
 		}
@@ -98,7 +100,7 @@ public class Apero {
 
 	public void setIdTarea_Apero(int idTarea_Apero) {
 		if(idTarea_Apero<1||idTarea_Apero>4){
-			JOptionPane.showMessageDialog(null, "El apero debe estar asignado a una tarea con id entre 1 y 4");
+			JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementWrongTask")); //$NON-NLS-1$
 		}else{
 			this.idTarea_Apero = idTarea_Apero;
 		}
@@ -120,7 +122,7 @@ public class Apero {
 
 	public void setIdUser(String idUser) {
 		if(idUser.length() != 9){ // El DNI del propietario debe tener el siguiente formato: ########L
-			JOptionPane.showMessageDialog(null, "El DNI del propietario debe tener el siguiente formato: ########-L");
+			JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementWrongOwner")); //$NON-NLS-1$
 		}else{
 			this.idUser = idUser;
 		}
@@ -132,33 +134,33 @@ public class Apero {
 	public Boolean validarDatos(String id, String nombre,String tamaño,String descrip,String tarea, String activo,String idUser){		
 		
 		if(idUser.length() != 9){ // El DNI del propietario debe tener el siguiente formato: ########-L
-			JOptionPane.showMessageDialog(null, "El DNI del propietario debe tener el siguiente formato: ########-L");
+			JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementWrongOwner")); //$NON-NLS-1$
 			return false;
 		}else{
 			Boolean r = isInteger(nombre);
 			
 			if(r.equals(true) || nombre.length() < 2 || nombre.length()>20){
-				JOptionPane.showMessageDialog(null, "Error. El nombre del apero no puede ser numerico ni estar vacío");
+				JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementNameNumericNorEmpty")); //$NON-NLS-1$
 				return false;
 				
 			}else{
 				int tam = Integer.parseInt(tamaño);
 				
 				if(!(isInteger(tamaño))||tam<=0||tam>500){
-					JOptionPane.showMessageDialog(null, "Error. El tamaño del apero no puede ser alfabético o menor que 0");
+					JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementSizeCharsNorNegative")); //$NON-NLS-1$
 					return false;
 				
 				}else{
 					if(descrip.length()>30||descrip.length()<=0){
-						JOptionPane.showMessageDialog(null, "La descripcion del apero debe constar de entre 0 y 30 carácteres alfanuméricos");
+						JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementWrongDesc")); //$NON-NLS-1$
 						return false;
 					}else{
 						int tar = Integer.parseInt(tarea);
 						if(tar<1||tar>4){
-							JOptionPane.showMessageDialog(null, "El apero debe estar asignado a una tarea");
+							JOptionPane.showMessageDialog(null, Idioma.getString("msgImplementNotTask")); //$NON-NLS-1$
 							return false;
 						}else{
-							System.out.println("Validacion OK!");
+							System.out.println("Validacion OK!"); //$NON-NLS-1$
 							return true;
 						}
 					}
