4d007d247732ba838a45da64b3e2e9b0e77db24f
eduardo.moret.morales@gmail.com <eduardo.moret.morales@gmail.com@1bd2956d-1e62-b8c4-f405-d16e971eae1f>
2011-12-21 17:59:59 +0000

    - Correción etiquetas - Añadido el idioma al correo de recuperar la contraseña en función del idioma del Login - Creado evento para que al hacer clic en el txtUser, borre el msg de "Inserte su DNI"

--- trunk/OpenGis/src/code/google/com/opengis/gestion/EnviarMail.java
+++ trunk/OpenGis/src/code/google/com/opengis/gestion/EnviarMail.java
@@ -9,6 +9,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.swing.JOptionPane;
 
+import code.google.com.opengis.gestionDAO.Idioma;
 import code.google.com.opengis.gestionDAO.LoginDao;
 
 
@@ -18,7 +19,7 @@ private String emailUsuario;
 private String passUsuario;
 private String nombre;
 private String apellido;
-	public EnviarMail(String user){
+	public EnviarMail(String user, String idioma){
 		try
 	        {
 			LoginDao ln = new LoginDao(user);
@@ -29,38 +30,39 @@ private String apellido;
 	            // Propiedades
 	            Properties prop = new Properties();
 	            //Host
-	            prop.setProperty("mail.smtp.host", "smtp.1and1.es");
+	            prop.setProperty("mail.smtp.host", "smtp.1and1.es"); //$NON-NLS-1$ //$NON-NLS-2$
 	            //Puerto
-	            prop.setProperty("mail.smtp.port", "587");
+	            prop.setProperty("mail.smtp.port", "587"); //$NON-NLS-1$ //$NON-NLS-2$
 	            //Usuario
-	            prop.setProperty("mail.smtp.user", "opengis@pipepito.es");
-	            prop.setProperty("mail.smtp.auth", "true");
+	            prop.setProperty("mail.smtp.user", "opengis@pipepito.es"); //$NON-NLS-1$ //$NON-NLS-2$
+	            prop.setProperty("mail.smtp.auth", "true"); //$NON-NLS-1$ //$NON-NLS-2$
 
 	            // Cargamos propiedades
 	            Session session = Session.getDefaultInstance(prop);
 
 	            // Mensaje
-	            if ( (emailUsuario == null) || (emailUsuario.equals("")) ){
-	            	JOptionPane.showMessageDialog(null,"Inserte un DNI Valido,si el DNI es correcto hable con el administrador");
+	            if ( (emailUsuario == null) || (emailUsuario.equals("")) ){ //$NON-NLS-1$
+	            	JOptionPane.showMessageDialog(null,Idioma.getString("msgMailValidId")); //$NON-NLS-1$
 	            }else{
 	            
 	            MimeMessage message = new MimeMessage(session);
-	            message.setFrom(new InternetAddress("opengis@pipepito.es"));
+	            message.setFrom(new InternetAddress("opengis@pipepito.es")); //$NON-NLS-1$
 	            message.addRecipient(
 	                Message.RecipientType.TO,
 	                //MAIL DEL USUARIO 
 	                new InternetAddress(emailUsuario));
-	            message.setSubject("Recuperación de clave");
+	            
+	            message.setSubject(Idioma.getString("msgMailPasswordRecovery")); //$NON-NLS-1$
 	           
 	            message.setText( 
-	            		"Hola "+nombre+" "+apellido+" tu contraseña es : "+passUsuario
+	            		Idioma.getString("msgMailHello")+nombre+" "+apellido+Idioma.getString("msgMailYourPassword")+passUsuario //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	                );
 
 	            // Envío
-	            Transport t = session.getTransport("smtp");
-	            t.connect("opengis@pipepito.es", "dai20112012");
+	            Transport t = session.getTransport("smtp"); //$NON-NLS-1$
+	            t.connect("opengis@pipepito.es", "dai20112012"); //$NON-NLS-1$ //$NON-NLS-2$
 	            t.sendMessage(message, message.getAllRecipients());
-	            JOptionPane.showMessageDialog(null,"se ha mandado un correo a "+emailUsuario+" compruébelo");
+	            JOptionPane.showMessageDialog(null,Idioma.getString("msgMailSent")+emailUsuario+Idioma.getString("msgMailCheck")); //$NON-NLS-1$ //$NON-NLS-2$
 
 	            t.close();}
 	        }
