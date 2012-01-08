817fed90b4039a6d26233d812c2b5c647e9af256
eduardo.moret.morales@gmail.com <eduardo.moret.morales@gmail.com@1bd2956d-1e62-b8c4-f405-d16e971eae1f>
2012-01-08 18:11:16 +0000

    Informe Dispositivos Traducido

--- trunk/OpenGis/src/code/google/com/opengis/gestion/InformeDispositivos.java
+++ trunk/OpenGis/src/code/google/com/opengis/gestion/InformeDispositivos.java
@@ -16,6 +16,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.crypto.Data;
 
 import code.google.com.opengis.gestionDAO.ConectarDBA;
+import code.google.com.opengis.gestionDAO.Idioma;
+
 import com.lowagie.text.*;
 
 import com.lowagie.text.Chunk;
@@ -49,16 +51,16 @@ static String fechaini;
 static String fechafin;
 
 public InformeDispositivos(String id) throws SQLException {
-	DatosUsuarioParcela(id,"01/01/2011","31/12/2011");
+	DatosUsuarioParcela(id,"01/01/2011","31/12/2011"); //$NON-NLS-1$ //$NON-NLS-2$
 	
-	crear_PDF("Informe Trabajador", "Opengis");
+	crear_PDF(Idioma.getString("_DeviceReport.Header"), Idioma.getString("_DeviceReport.Author")); //$NON-NLS-1$ //$NON-NLS-2$
 
 }
 public void DatosUsuarioParcela(String id,String inicio,String fin) {
 	fechaini = inicio;
 	fechafin = fin; //implementar la consulta para elegir entre 2 fechas
-	consulta = "SELECT `dispositivo`.`modelo` , `dispositivo`.`num_serie` , `dispositivo`.`iddispositivo` FROM dispositivo WHERE ((`dispositivo`.`iddispositivo` LIKE '"+id+"'))";
-	consulta2 = "SELECT `usuario`.`nombre`, `usuario`.`apellidos`, `usuario`.`dni` , `prestamo`.`fecha_alquiler` , `prestamo`.`fecha_devol` FROM dispositivo, prestamo, usuario	WHERE ((`dispositivo`.`iddispositivo` LIKE '"+id+"') AND (`prestamo`.`iddispositivo` LIKE `dispositivo`.`iddispositivo`) AND (`usuario`.`dni` LIKE `prestamo`.`dni_usuario`))";
+	consulta = "SELECT `dispositivo`.`modelo` , `dispositivo`.`num_serie` , `dispositivo`.`iddispositivo` FROM dispositivo WHERE ((`dispositivo`.`iddispositivo` LIKE '"+id+"'))"; //$NON-NLS-1$ //$NON-NLS-2$
+	consulta2 = "SELECT `usuario`.`nombre`, `usuario`.`apellidos`, `usuario`.`dni` , `prestamo`.`fecha_alquiler` , `prestamo`.`fecha_devol` FROM dispositivo, prestamo, usuario	WHERE ((`dispositivo`.`iddispositivo` LIKE '"+id+"') AND (`prestamo`.`iddispositivo` LIKE `dispositivo`.`iddispositivo`) AND (`usuario`.`dni` LIKE `prestamo`.`dni_usuario`))"; //$NON-NLS-1$ //$NON-NLS-2$
 			 	ConectarDBA.acceder();
 	
 	try {
@@ -88,15 +90,15 @@ private void crear_PDF(String titulo, String n) {
 	if(this.ruta_destino!=null){
 		try {	
 			mipdf = new Document (PageSize.LEGAL.rotate());
-			PdfWriter.getInstance(mipdf, new FileOutputStream(this.ruta_destino + ".pdf")).setInitialLeading(6);
+			PdfWriter.getInstance(mipdf, new FileOutputStream(this.ruta_destino + ".pdf")).setInitialLeading(6); //$NON-NLS-1$
 			mipdf.open();// se abre el documento
             mipdf.addTitle(titulo); // se añade el titulo
             mipdf.addAuthor(n); // se añade el autor del documento
-            mipdf.addSubject(""); //se añade el asunto del documento
-            mipdf.addKeywords(""); //Se agregan palabras claves 
-            Paragraph nn = new Paragraph("Informe Trabajador", FontFactory.getFont("arial",22,Font.BOLD));    
+            mipdf.addSubject(""); //se añade el asunto del documento //$NON-NLS-1$
+            mipdf.addKeywords(""); //Se agregan palabras claves  //$NON-NLS-1$
+            Paragraph nn = new Paragraph(Idioma.getString("_DeviceReport.Header"), FontFactory.getFont("arial",22,Font.BOLD));     //$NON-NLS-1$ //$NON-NLS-2$
             nn.setAlignment(Element.ALIGN_CENTER);
-            mipdf.add(new Paragraph(" ", FontFactory.getFont("arial",22,Font.BOLD)));             
+            mipdf.add(new Paragraph(" ", FontFactory.getFont("arial",22,Font.BOLD)));              //$NON-NLS-1$ //$NON-NLS-2$
             mipdf.add(nn);
            
           /*  Image foto = Image.getInstance("pingu.png");
@@ -104,21 +106,21 @@ private void crear_PDF(String titulo, String n) {
             foto.setAlignment(Chunk.ALIGN_MIDDLE);
             mipdf.add(foto); 
             */
-            mipdf.add(new Paragraph("Modelo: "+ modelo, FontFactory.getFont("arial",22,Font.BOLD)));             
-            mipdf.add(new Paragraph("Numero de Serie: "+ nºserie, FontFactory.getFont("arial",22,Font.BOLD)));             
-            mipdf.add(new Paragraph("ID dispositivo: "+ iddispositivo, FontFactory.getFont("arial",22,Font.BOLD)));             
+            mipdf.add(new Paragraph(Idioma.getString("_DeviceReport.Model")+ modelo, FontFactory.getFont("arial",22,Font.BOLD)));              //$NON-NLS-1$ //$NON-NLS-2$
+            mipdf.add(new Paragraph(Idioma.getString("_DeviceReport.SerialNumber")+ nºserie, FontFactory.getFont("arial",22,Font.BOLD)));              //$NON-NLS-1$ //$NON-NLS-2$
+            mipdf.add(new Paragraph(Idioma.getString("_DeviceReport.DeviceID")+ iddispositivo, FontFactory.getFont("arial",22,Font.BOLD)));              //$NON-NLS-1$ //$NON-NLS-2$
                  
-            mipdf.add(new Paragraph(" ", FontFactory.getFont("arial",22,Font.BOLD)));             
+            mipdf.add(new Paragraph(" ", FontFactory.getFont("arial",22,Font.BOLD)));              //$NON-NLS-1$ //$NON-NLS-2$
 
             //mipdf.add(new Paragraph("Dispositivos usados entre "+ fechaini +" y "+fechafin, FontFactory.getFont("arial",22,Font.BOLD)));             
-            mipdf.add(new Paragraph(" ", FontFactory.getFont("arial",22,Font.BOLD)));             
+            mipdf.add(new Paragraph(" ", FontFactory.getFont("arial",22,Font.BOLD)));              //$NON-NLS-1$ //$NON-NLS-2$
             PdfPTable tabla = new PdfPTable(5);
             tabla.setHorizontalAlignment(Element.ALIGN_CENTER);
-            tabla.addCell("Nombre trabajador");
-            tabla.addCell("Apellidos");
-            tabla.addCell("Dni");
-            tabla.addCell("Fecha inicio");
-            tabla.addCell("Fecha final");
+            tabla.addCell(Idioma.getString("_DeviceReport.WorkerName")); //$NON-NLS-1$
+            tabla.addCell(Idioma.getString("_DeviceReport.LastName")); //$NON-NLS-1$
+            tabla.addCell(Idioma.getString("_DeviceReport.IDCard")); //$NON-NLS-1$
+            tabla.addCell(Idioma.getString("_DeviceReport.IniDate")); //$NON-NLS-1$
+            tabla.addCell(Idioma.getString("_DeviceReport.EndDate")); //$NON-NLS-1$
             
             try {
 				while(rs.next()){
@@ -131,7 +133,7 @@ private void crear_PDF(String titulo, String n) {
 			}
             mipdf.add(tabla);
             mipdf.close(); //se cierra el PDF&
-            JOptionPane.showMessageDialog(null,"Documento PDF creado");
+            JOptionPane.showMessageDialog(null,Idioma.getString("_DeviceReport.DocCreated")); //$NON-NLS-1$
             
             
 		} catch (FileNotFoundException e) {
@@ -148,7 +150,7 @@ private void crear_PDF(String titulo, String n) {
 
 public void Colocar_Destino() {
 	FileNameExtensionFilter filter = new FileNameExtensionFilter(
-			"Archivo PDF", "pdf", "PDF");
+			Idioma.getString("_DeviceReport.PDFFile"), "pdf", "PDF"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	JFileChooser fileChooser = new JFileChooser();
 	fileChooser.setFileFilter(filter);
 	int result = fileChooser.showSaveDialog(null);
