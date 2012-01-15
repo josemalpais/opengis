82d78486d50aa2f323bb4546645dd723424c1c1f
jugade92 <jugade92@gmail.com>
2012-01-15 12:30:41 +0000

    Ya no se puede modificar un prestamo inactivo aunque te permita darle al botón modificar

--- OpenGis/src/code/google/com/opengis/gestionVISUAL/PrestamoPanelPrincipal.java
+++ OpenGis/src/code/google/com/opengis/gestionVISUAL/PrestamoPanelPrincipal.java
@@ -84,9 +84,24 @@ public class PrestamoPanelPrincipal extends GeneradorPanelPrincipal{
 				rPrestamo[i] = getTablaPrincipal().getValueAt(fila, i)
 						.toString();
 			}
-			PrestamoPanelGestion p = new PrestamoPanelGestion("modificar",rPrestamo[0].toString(),rPrestamo[1].toString(),rPrestamo[2].toString(), rPrestamo[1].toString()); // Creamos el panel de Modificación de Préstamo //$NON-NLS-1$
 			
-			VentanaPrincipal.añadirPestañaNueva(Idioma.getString("etModifyLoan")+"("+rPrestamo[1].toString()+")",p); // Añadimos el panel a la pestaña //$NON-NLS-1$
+			
+			if(rPrestamo[4].equals("no")){
+				
+				PrestamoPanelGestion p = new PrestamoPanelGestion("modificar",rPrestamo[0].toString(),rPrestamo[1].toString(),rPrestamo[2].toString(), rPrestamo[1].toString()); // Creamos el panel de Modificación de Préstamo //$NON-NLS-1$
+				
+				VentanaPrincipal.añadirPestañaNueva(Idioma.getString("etModifyLoan")+"("+rPrestamo[1].toString()+")",p); // Añadimos el panel a la pestaña //$NON-NLS-1$
+				
+			}else{
+				
+				JOptionPane.showMessageDialog(null,Idioma.getString("msgLoanAlreadyReturned"));
+				bModificar.setEnabled(false);
+				bEliminar.setEnabled(false);
+				
+				
+			}
+			
+			
 		}
 		
 	}
